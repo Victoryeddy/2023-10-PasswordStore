5d1ef1accd0d9dfd5be009bf631c52a02039e482
Victory Ndu <victor5ndu@gmail.com>
2023-10-23 09:17:17 +0100

    Update Contract and add more test

--- test/PasswordStore.t.sol
+++ test/PasswordStore.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-import {Test, console2} from "../lib/forge-std/src/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {PasswordStore} from "../src/PasswordStore.sol";
 import {DeployPasswordStore} from "../script/DeployPasswordStore.s.sol";
 
